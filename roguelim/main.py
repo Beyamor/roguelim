e7958e00027874fc833ab76dfd9e76f0432ee63b
beyamor <beyamor@gmail.com>
2013-11-26 16:55:26 -0800

    Reading direction commands

--- roguelim/main.py
+++ roguelim/main.py
@@ -1,11 +1,28 @@
 from dungeon import Dungeon
 
+DIRECTION_COMMANDS = {
+		'north':	'north',
+		'up':		'north',
+		'east':		'east',
+		'right':	'east',
+		'south':	'south',
+		'down':		'south',
+		'west':		'west',
+		'left':		'west'
+		}
+
 def main():
 	dungeon = Dungeon()
 	while True:
-		line = raw_input()
-		print("Got " + line) 
-		print(dungeon)
+		line	= raw_input()
+		command	= line.lower().strip()
+
+		if command in DIRECTION_COMMANDS:
+			direction = DIRECTION_COMMANDS[command]
+			print("Moving " + direction)
+			print(dungeon)
+		else:
+			print("Unrecognized command: " + command)
 
 if __name__ == "__main__":
 	main()
