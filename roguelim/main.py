c4f8ac85cbfb2ac37b4502107a712b882f82698d
beyamor <beyamor@gmail.com>
2013-11-26 16:25:25 -0800

    An actual dungeon class

--- roguelim/main.py
+++ roguelim/main.py
@@ -5,26 +5,26 @@ DUNGEON_HEIGHT	= 10
 FLOOR_TILE	= "="
 WALL_TILE	= "#"
 
-def create_dungeon():
-	dungeon = [[None for y in range(DUNGEON_HEIGHT)] for x in range(DUNGEON_WIDTH)]
-	for x in range(DUNGEON_WIDTH):
-		for y in range(DUNGEON_HEIGHT):
-			dungeon[x][y] = random.choice([WALL_TILE, FLOOR_TILE])
-
-	return dungeon
-
-def dungeon_string(dungeon):
-	s = ""
-	for y in range(DUNGEON_HEIGHT):
+class Dungeon:
+	def __init__(self):
+		self.tiles = [[None for y in range(DUNGEON_HEIGHT)] for x in range(DUNGEON_WIDTH)]
 		for x in range(DUNGEON_WIDTH):
-			s = s + dungeon[x][y]
+			for y in range(DUNGEON_HEIGHT):
+				self.tiles[x][y] = random.choice([WALL_TILE, FLOOR_TILE])
 
-		if y < DUNGEON_HEIGHT - 1:
-			s = s + "\n"
-	return s
+	def __str__(self):
+		s = ""
+		for y in range(DUNGEON_HEIGHT):
+			for x in range(DUNGEON_WIDTH):
+				s = s + self.tiles[x][y]
+
+			if y < DUNGEON_HEIGHT - 1:
+				s = s + "\n"
+		return s
 
 def main():
-	print(dungeon_string(create_dungeon()))
+	dungeon = Dungeon()
+	print(str(dungeon))
 
 if __name__ == "__main__":
 	main()
